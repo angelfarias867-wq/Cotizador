e519780a567cf06435e361ab88250d284360cb6b
angelfarias867-wq <angelfarias867@gmail.com>
2026-04-28 14:26:58 -0400

    primer commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const form = document.querySelector('#coin-form'); 
+const coin = document.querySelector('#coin');
+const crypto = document.querySelector('#crypto');
+const amount = document.querySelector('#amount');
+const coinInfo = document.querySelector('#coin-info')
+
+form.addEventListener('submit', async e => {
+    e.preventDefault(); //que hace el preventDeFault
+    const coinSelected = [...coin.children].find(option => option.selected);
+    const cryptoSelected = [...crypto.children].find(option => option.selected);
+    const amountValue = amount.value;
+
+    try {
+        coinInfo.innerHTML = `
+        <span class="loader"></span>
+        `;
+        const response = await (await fetch(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cryptoSelected}&tsyms=${coinSelected}`)).json();
+        const price = response.DISPLAY[cryptoSelected.value][coinSelected.value].PRICE;
+        const priceHigh = response.DISPLAY[cryptoSelected.value][coinSelected.value].HIGH24HOUR;
+        const priceLow = response.DISPLAY[cryptoSelected.value][coinSelected.value].LOW24HOUR;
+        const variation = response.DISPLAY[cryptoSelected.value][coinSelected.value].CHANGEPCT24HOUR;
+
+        if (amountValue !== '') {
+            const result = Number(amountValue) / response.RAW[cryptoSelected][coinSelected].PRICE;
+            coinInfo.innerHTML = `
+            <p class="info">El precio es: <span class="price">${price}</span></p>
+            <p class="info">El precio más alto es: <span class="price">${priceHigh}</span></p>
+            <p class="info">El precio más bajo es: <span class="price">${priceLow}</span></p>
+            <p class="info">Variación 24H: <span class="price"></span>${variation}%</p>
+            <p class="info">Puede Comprar: <span class="price">${result.toFixed(4)} ${cryptoSelected}</span></p>
+            `
+        } else{
+            coinInfo.innerHTML = `
+            <p class="info">El precio es: <span class="price">${price}</span></p>
+            <p class="info">El precio más alto es: <span class="price">${priceHigh}</span></p>
+            <p class="info">El precio más bajo es: <span class="price">${priceLow}</span></p>
+            <p class="info">Variación 24H: <span class="price"></span>${variation}%</p>
+        `};
+
+    } catch (error) {
+        console.log(error);
+    }
+});
